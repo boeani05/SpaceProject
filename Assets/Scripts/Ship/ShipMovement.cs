697d9442b31af7247b1704cf7cfa3c070b93650f
boeani05 <flyingdoge@gmx.at>
2026-05-20 20:08:00 +0200

    added first Movement to Game

--- Assets/Scripts/Ship/ShipMovement.cs
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour
 {
+    [SerializeField] new Rigidbody2D rigidbody;
+    [SerializeField] float movementSpeed;
+
     private float horizontalInput;
     private float verticalInput;
-    private float movementSpeed;
     private bool isFacingRight;
     private bool isFacingUp;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,6 +18,76 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+        horizontalInput = GetHorizontalAxis();
+        verticalInput = GetVerticalAxis();
+
+        Flip();
+    }
+
+        private float GetHorizontalAxis()
+    {
+        return Input.GetAxisRaw("Horizontal");
+    }
+
+    private float GetVerticalAxis()
+    {
+        return Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.linearVelocity = SetLinearVelocityOfRigidbody();
+    }
+
+    private void Flip()
+    {
+        FlipHorizontallyIfNeeded();
+
+        FlipVerticallyIfNeeded();
+    }
+
+    private void FlipHorizontallyIfNeeded()
+    {
+        if (!ShouldFlip(isFacingRight, horizontalInput)) return;
+
+        isFacingRight = !isFacingRight;
+
+        FlipX();
+    }
+
+    private void FlipVerticallyIfNeeded()
+    {
+        if (!ShouldFlip(isFacingUp, verticalInput)) return;
+
+        isFacingUp = !isFacingUp;
+
+        FlipY();
+    }
+
+    private bool ShouldFlip(bool isFacingPositiveDirection, float directionalInput)
+    {
+        return isFacingPositiveDirection && directionalInput < 0 ||
+               !isFacingPositiveDirection && directionalInput > 0;
+    }
+
+    private void FlipX()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+
+        transform.localScale = scale;
+    }
+
+    private void FlipY()
+    {
+        Vector3 scale = transform.localScale;
+        scale.y *= -1;
+
+        transform.localScale = scale;
+    }
+
+    private Vector2 SetLinearVelocityOfRigidbody()
+    {
+        return new Vector2(horizontalInput, verticalInput).normalized * movementSpeed;
     }
 }
